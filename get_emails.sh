9784d748bd512c993e54ad732d8f76e3ec2d4bb8
Alex <62762106+JudgeMegapolis@users.noreply.github.com>
2022-03-30 10:22:18 -0400

    Update get_emails.sh

--- get_emails.sh
+++ get_emails.sh
@@ -3,5 +3,5 @@
 #directory=$(`dirname "$0"`)
 #directory=$("$(dirname "${BASH_SOURCE[0]}")")
 #echo "$directory"
-zcat "$(dirname "${BASH_SOURCE[0]}")"/**/**/*.gz | grep -a "$1" # either im stupid or this is harder than it should be to get the fucking directory of the script. baka. owo
+zcat $(dirname "${BASH_SOURCE[0]}")/**/**/*.gz | grep -a "$1" # either im stupid or this is harder than it should be to get the fucking directory of the script. baka. owo
 
